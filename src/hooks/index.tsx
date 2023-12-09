fb96439af01120421ba051ad34141dc34ee495df
chezee4 <vadimdufinec@gmail.com>
2023-12-09 15:53:16 +0200

    added hook useHttp

---
+++ src/hooks/index.tsx
@@ -0,0 +1,39 @@
+import { useCallback } from "react";
+
+export const useHttp = () => {
+  const BASE_URL = process.env.NEXT_APP_FETCH_TEMPLATE;
+  const request = useCallback(
+    async (
+      path = "",
+      method = "GET",
+      body: string | null = null,
+      headers = {
+        "Content-Type": "application/json",
+        accepts: "application/json",
+      }
+    ) => {
+      try {
+        const response = await fetch(`${BASE_URL}${path}`, {
+          method,
+          body,
+          headers,
+        });
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw (
+            data.message ||
+            `Could not fetch ${BASE_URL}${path}, status: ${response.status}`
+          );
+        }
+
+        return data;
+      } catch (e) {
+        throw e;
+      }
+    },
+    []
+  );
+
+  return { request };
+};
