1ca2fc40ad8c5bae19dfadfef76de1a71db570ba
chezee4 <vadimdufinec@gmail.com>
2024-01-04 20:19:47 +0200

    fixing a bug with slider id

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -7,37 +7,53 @@ import Chap from '@/components/ui/chap'
 
 import { AiFillClockCircle } from 'react-icons/ai'
 import { IoEyeSharp } from 'react-icons/io5'
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa6"
+import { FaArrowRight, FaArrowLeft } from 'react-icons/fa6'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import { cn } from '@/lib/utils'
 
 type CaruselProps = {
   title: string
+  text?: string
 }
-export default function Carusel({title}:CaruselProps) {
+export default function Carusel({ title, text }: CaruselProps) {
   return (
     <div className=" w-full text-white max-w-[1450px] mb-16 m-auto ">
-     <div className='flex justify-between items-center mb-8'>
-      <h2 className='text-white text-3xl leading-[150%] font-bold'>{title}</h2>
-     <div className='flex items-center gap-3 rounded-xl border-black-12 border bg-black-6 p-4'>
-        <button className="prev-slide-button rounded-lg border-black-12 border bg-black-10 text-xl p-3 transition-all duration-200 ease-linear hover:bg-black-15"><FaArrowLeft/></button>
-        <div className="swiper-custom-pagination"></div>
-        <button className="next-slide-button rounded-lg border-black-12 border bg-black-10 text-xl p-3 transition-all duration-200 ease-linear  hover:bg-black-15"><FaArrowRight/></button>
+      <div className="flex justify-between items-center mb-8">
+        <h2 className="text-white text-3xl leading-[150%] font-bold">{title}</h2>
+        <div className="flex items-center gap-3 rounded-xl border-black-12 border bg-black-6 p-4">
+          <button
+            className={cn(
+              'rounded-lg border-black-12 border bg-black-10 text-xl p-3 transition-all duration-200 ease-linear hover:bg-black-15',
+              `prev-${text}-button`,
+            )}
+          >
+            <FaArrowLeft />
+          </button>
+          <div className={cn(text, 'flex gap-1')}></div>
+          <button
+            className={cn(
+              'rounded-lg border-black-12 border bg-black-10 text-xl p-3 transition-all duration-200 ease-linear  hover:bg-black-15',
+              `next-${text}-button`,
+            )}
+          >
+            <FaArrowRight />
+          </button>
+        </div>
       </div>
-     </div>
       <Swiper
         modules={[Navigation, Pagination, A11y]}
         spaceBetween={20}
         slidesPerView={5}
         navigation={{
-          nextEl: '.next-slide-button',
-          prevEl: '.prev-slide-button',
+          nextEl: `.next-${text}-button`,
+          prevEl: `.prev-${text}-button`,
         }}
-        pagination={{el:'.swiper-custom-pagination', clickable: true }}
+        pagination={{ el: '.' + text, clickable: true }}
         breakpoints={{
           320: {
             slidesPerView: 1,
--- src/components/movies-by-category.tsx
+++ src/components/movies-by-category.tsx
@@ -6,9 +6,9 @@ export default function MoviesByCategory() {
     <section className=' relative rounded-xl border border-black-15 w-full mb-10 '>
        <Chap className=" absolute translate-x-1/2 -translate-y-1/2 bg-red-45 rounded-lg text-lg text-white font-normal py-1.5 px-4">Movies</Chap>
        <div className="mt-14">
-       <Carusel title="Trending Now"/>
-       <Carusel title="Our Genres"/>
-       <Carusel title="New Releases"/>
+       <Carusel title="Trending Now" text="pagination-1"/>
+       <Carusel title="Our Genres" text="pagination-2"/>
+       <Carusel title="New Releases" text="pagination-3"/>
        </div>
     </section>
   )
