fe143ea89add908b1f3c37f1618f1feb120d2820
chezee4 <vadimdufinec@gmail.com>
2024-01-29 21:12:15 +0200

    create-footer

---
+++ src/config/footer-navigation.ts
@@ -0,0 +1,45 @@
+export const footerLinks = [
+  {
+    id: '1',
+    title: 'Home',
+    links: [
+      { id: 'Categories', title: 'Categories', path: '/' },
+      { id: 'Devices', title: 'Devices', path: '/' },
+      { id: 'Pricing', title: 'Pricing', path: '/' },
+      { id: 'FAQ', title: 'FAQ', path: '/' },
+    ],
+  },
+  {
+    id: '2',
+    title: 'Movies',
+    links: [
+      { id: 'Gernes', title: 'Gernes', path: '/' },
+      { id: 'Trending', title: 'Trending', path: '/' },
+      { id: 'New Release', title: 'New Release', path: '/' },
+      { id: 'Popular', title: 'Popular', path: '/' },
+    ],
+  },
+  {
+    id: '3',
+    title: 'Shows',
+    links: [
+      { id: 'Gernes', title: 'Gernes', path: '/' },
+      { id: 'Trending', title: 'Trending', path: '/' },
+      { id: 'New Release', title: 'New Release', path: '/' },
+      { id: 'Popular', title: 'Popular', path: '/' },
+    ],
+  },
+  {
+    id: '4',
+    title: 'Support',
+    links: [{ id: 'Contact Us', title: 'Contact Us', path: '/' }],
+  },
+  {
+    id: '5',
+    title: 'Subscription',
+    links: [
+      { id: 'Plans', title: 'Plans', path: '/' },
+      { id: 'Features', title: 'Features', path: '/' },
+    ],
+  },
+] as const
