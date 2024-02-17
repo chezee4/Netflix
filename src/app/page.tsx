70f616ee140c83bcb02d7f32f870ec04dea47203
chezee4 <vadimdufinec@gmail.com>
2024-02-17 22:06:20 +0200

    create accordion for support page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AccordionSupport from '@/components/accordion-support'
 import CarouselAvatars from '@/components/carousel-avatars'
 import CarouselComment from '@/components/carousel-comment'
 import CTA from '@/components/cta'
@@ -8,11 +9,12 @@ import ServiceList from '@/components/services-list'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between w-full max-w-[1640px] px-3 md:px-8 m-auto">
+    <main className="flex min-h-screen flex-col items-center gap-10 justify-between w-full max-w-[1640px] px-3 md:px-8 m-auto">
       <DevicesList />
       <CTA />
       <CarouselAvatars />
       <CarouselComment />
+      <AccordionSupport />
       <ServiceList />
       <MoviesByCategory />
       {/* <SupportForm /> */}
