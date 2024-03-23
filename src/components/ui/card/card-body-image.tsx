97d4355adecee38336a20005c300e7d2c64343cc
chezee4 <vadimdufinec@gmail.com>
2024-03-23 16:18:55 +0200

    rewrote carousel movies category to shadcn/ui

--- src/components/ui/card/card-body-image.tsx
+++ src/components/ui/card/card-body-image.tsx
@@ -3,23 +3,18 @@ import Image from 'next/image'
 import React from 'react'
 
 type CardBodyImageProps = {
-  url: any
+  url: any,
   className?: string
 }
 
-export default function CardBodyImage({ url, className }: CardBodyImageProps) {
+export default function CardBodyImage({ url,className }: CardBodyImageProps) {
   return (
-    <div
-      className={cn(
-        'w-full m-auto h-full max-w-[240px] max-h-[270px] mb-2 rounded-lg overflow-hidden',
-        className,
-      )}
-    >
+    <div className={cn('w-full m-auto h-full max-w-[240px] max-h-[270px] mb-2 rounded-lg overflow-hidden',className)}>
       <Image
         src={url}
         width={250}
         height={320}
-        className="rounded-lg object-bottom-bottom object-cover "
+        className='rounded-lg object-bottom-bottom object-cover '
         alt="card"
       ></Image>
     </div>
