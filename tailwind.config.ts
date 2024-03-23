a109007af7dbf55aa9efb763c6a5b5577c7b0eb9
chezee4 <vadimdufinec@gmail.com>
2024-03-23 10:55:07 +0200

    configured the project for shadcn/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,65 @@
 import type { Config } from 'tailwindcss'
-const config: Config = {
+
+const config = {
+  darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
+      backgroundImage: theme => ({
+        'gradient-linear':
+          'linear-gradient(90deg, rgba(90,9,9,0.15738795518207283) 2%, rgba(172,9,9,0.8548669467787114) 19%, rgba(138,13,13,0.8856792717086834) 42%, rgba(110,11,11,0.36747198879551823) 72%, rgba(110,11,11,0.12097338935574231) 94%)',
+        'bg-cva': "url('../../public/bgCTA.png')",
+      }),
       backgroundColor: {
         primary: '#141414',
       },
-      backgroundImage: {
-        "bg-cva": "url('../../public/bgCTA.png')",
-      },
       colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
         gray: {
           60: '#999999',
           65: '#A6A6A6',
@@ -35,7 +81,7 @@ const config: Config = {
           35: '#4C4C4C',
         },
         red: {
-          "hover": "#c10b0b",
+          hover: '#c10b0b',
           45: '#E50000',
           50: '#FF0000',
           55: '#FF1919',
@@ -47,11 +93,31 @@ const config: Config = {
         },
       },
       screens: {
-        mm:"400px",
-        xs: "480px",
+        mm: '400px',
+        xs: '480px',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
-    plugins: [],
   },
-}
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config
+
 export default config
