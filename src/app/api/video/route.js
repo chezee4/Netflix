96e4a68c102dacd65cbb4fd724a83e00689c827a
chezee4 <vadimdufinec@gmail.com>
2024-03-30 08:50:43 +0200

    test next-video

--- src/app/api/video/route.js
+++ src/app/api/video/route.js
@@ -1,1 +1,20 @@
 export { GET } from 'next-video/request-handler'
+import fs from 'fs'
+import path from 'path'
+
+export function handler(req, res) {
+  const {
+    query: { video },
+  } = req
+
+  const filePath = path.join(process.cwd(), 'videos', `${video}.json`)
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).json({ error: 'Ошибка чтения файла' })
+    }
+
+    res.status(200).json(JSON.parse(data))
+  })
+}
