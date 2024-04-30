5cb6b94d3da00d9cef5e1e2578660e0af9cfad9e
MakShon <maksym.baran69@gmail.com>
2024-04-30 13:02:35 +0300

    fix/ changed endpoint to upload movie

--- src/main/java/com/example/netflixapi/controller/MovieController.java
+++ src/main/java/com/example/netflixapi/controller/MovieController.java
@@ -1,8 +1,10 @@
 package com.example.netflixapi.controller;
 
 import com.example.netflixapi.model.Movie;
+import com.example.netflixapi.service.MovieMediaService;
 import com.example.netflixapi.service.MovieService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,8 +14,10 @@ import java.util.UUID;
 @RequestMapping("/api/movies")
 public class MovieController {
     private final MovieService movieService;
+    private final MovieMediaService movieMediaService;
 
-    public MovieController(MovieService movieService) {
+    public MovieController(MovieService movieService, MovieMediaService movieMediaService) {
+        this.movieMediaService = movieMediaService;
         this.movieService = movieService;
     }
     @GetMapping
@@ -22,8 +26,21 @@ public class MovieController {
     @GetMapping("/{id}")
     public Optional<Movie> findById(@PathVariable UUID id){ return movieService.findById(id); }
 
-    @PostMapping
-    public Movie saveMovie(@RequestBody Movie movie){ return movieService.saveMovie(movie); }
+    @PostMapping("/upload")
+    public Movie uploadMovieWithMedia(@RequestParam("name") String name,
+                                      @RequestParam("alt") String alt,
+                                      @RequestParam("photo") MultipartFile photo,
+                                      @RequestParam("video") MultipartFile video) {
+        Movie movie = new Movie();
+        movie.setName(name);
+        movie.setAlt(alt);
+
+        movie = movieService.saveMovie(movie);
+
+        movie = movieMediaService.uploadMedia(movie.getId(), photo, video);
+
+        return movie;
+    }
 
     @PutMapping
     public Movie updateMovie(@RequestBody Movie movie){ return movieService.updateMovie(movie); }
