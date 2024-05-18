21e1e97b20d28a691ae522a96895b81b27e87d15
chezee4 <dufynets.vadym@student.uzhnu.edu.ua>
2024-05-18 23:49:01 +0300

    fix build

--- src/config/card/card-film.ts
+++ src/config/card/card-film.ts
@@ -1,57 +1,57 @@
-import IMG1 from 'src/../public/IMG1.jpg'
-import IMG2 from 'src/../public/IMG2.jpg'
-import IMG3 from 'src/../public/IMG3.jpg'
-import IMG4 from 'src/../public/IMG4.jpg'
+// import IMG1 from 'src/../public/IMG1.jpg'
+// import IMG2 from 'src/../public/IMG2.jpg'
+// import IMG3 from 'src/../public/IMG3.jpg'
+// import IMG4 from 'src/../public/IMG4.jpg'
 
-import { Film } from 'src/types'
+// import { Film } from 'src/types'
 
-export const list: Film[] = [
-  {
-    title: 'Втеча з Шоушенка',
-    duration: '45хв',
-    img: IMG1,
-    numberOfViews: '2K',
-  },
-  {
-    title: 'Це чудове життя',
-    duration: '1год 30хв',
-    img: IMG2,
-    numberOfViews: '3K',
-  },
-  {
-    title: 'Назад у майбутнє',
-    duration: '1год',
-    img: IMG3,
-    numberOfViews: '1.5K',
-  },
-  {
-    title: 'Звичайні підозрювані',
-    duration: '2год 10хв',
-    img: IMG4,
-    numberOfViews: '860',
-  },
-  {
-    title: 'Свідок обвинувачення',
-    duration: '1год 10хв',
-    img: IMG2,
-    numberOfViews: '2.5k',
-  },
-  {
-    title: 'Назад у майбутнє',
-    duration: '50хв',
-    img: IMG4,
-    numberOfViews: '160',
-  },
-  {
-    title: 'Темний лицар повертається',
-    duration: '2год 10хв',
-    img: IMG3,
-    numberOfViews: '860',
-  },
-  {
-    title: 'Це чудове життя',
-    duration: '5год 10хв',
-    img: IMG2,
-    numberOfViews: '2.3K',
-  },
-]
+// export const list: Film[] = [
+//   {
+//     title: 'Втеча з Шоушенка',
+//     duration: 45,
+//     img: IMG1,
+//     numberOfViews: '2K',
+//   },
+//   {
+//     title: 'Це чудове життя',
+//     duration: 130,
+//     img: IMG2,
+//     numberOfViews: '3K',
+//   },
+//   {
+//     title: 'Назад у майбутнє',
+//     duration: 60,
+//     img: IMG3,
+//     numberOfViews: '1.5K',
+//   },
+//   {
+//     title: 'Звичайні підозрювані',
+//     duration: 180,
+//     img: IMG4,
+//     numberOfViews: '860',
+//   },
+//   {
+//     title: 'Свідок обвинувачення',
+//     duration: 200,
+//     img: IMG2,
+//     numberOfViews: '2.5k',
+//   },
+//   {
+//     title: 'Назад у майбутнє',
+//     duration: 120,
+//     banner: IMG4,
+//     numberOfViews: '160',
+//   },
+//   {
+//     title: 'Темний лицар повертається',
+//     duration: 210,
+//     img: IMG3,
+//     numberOfViews: '860',
+//   },
+//   {
+//     title: 'Це чудове життя',
+//     duration: 300,
+//     img: IMG2,
+//     numberOfViews: '2.3K',
+//   },
+// ]
