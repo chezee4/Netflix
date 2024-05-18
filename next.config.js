b32e429bd458666de980db2a6fcc9ad1326f9c75
chezee4 <dufynets.vadym@student.uzhnu.edu.ua>
2024-05-18 23:39:41 +0300

    fix build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 // next.config.js
-const { withNextVideo } = require('next-video/process')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -11,16 +10,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withNextVideo(nextConfig, {
-  provider: 'amazon-s3',
-
-  providerConfig: {
-    'amazon-s3': {
-      accessKeyId: 'AKIA2UC3DSKSD4E75R3K',
-      secretAccessKey: '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',
-      region: 'eu-north-1',
-      bucket: 'makshon-netflix-api-bucket',
-      endpoint: 'https://s3.eu-north-1.amazonaws.com',
-    },
-  },
-})
+module.exports = nextConfig
